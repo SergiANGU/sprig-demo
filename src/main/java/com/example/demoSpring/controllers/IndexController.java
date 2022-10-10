099cd9bc17efe5d1b2ae5c063f55f56dc3d4d5f6
sergivi11 <sergivi04@gmail.com>
2022-10-10 12:04:00 +0200

    Spring perfil de usuario

--- src/main/java/com/example/demoSpring/controllers/IndexController.java
+++ src/main/java/com/example/demoSpring/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demoSpring.controllers;
 
+import com.example.demoSpring.Models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-    @GetMapping({"/index", "/", "/home"})
+    @GetMapping({"/index", "/", "/home", ""})
     public String index(Model model) {
         model.addAttribute("titulo", "Hola spring framework");
 
         return"index";
+    }
+    @RequestMapping("/perfil")
+    public String perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Sergi");
+        usuario.setApellido("Andres");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "perfil del usuario: " .concat(usuario.getNombre()));
+        return "perfil";
     }
 
 }
